e4821eebc70d5a4feb175dfa1577e7c94636e6c3
jp <josepablomtz@me.com>
2020-04-29 20:24:24 -0700

    Set password fields.

---
+++ app/src/main/java/com/example/appsmovav01/Inicio.kt
@@ -0,0 +1,21 @@
+package com.example.appsmovav01
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main1.*
+
+class Inicio : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main1)
+
+        logo_helu.setOnClickListener(View.OnClickListener {
+            var intent = Intent(this, pruebas::class.java)
+            startActivity(intent)
+        })
+
+    }
+}
---
+++ app/src/main/java/com/example/appsmovav01/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.appsmovav01.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+
+}
